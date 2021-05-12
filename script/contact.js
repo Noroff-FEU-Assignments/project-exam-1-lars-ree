be4aa2c3f112c9f30203619f8c8cb5911c7dc7f3
lars-ree <70200269+lars-ree@users.noreply.github.com>
2021-05-12 21:58:21 +0200

    Validation contact form

--- script/contact.js
+++ script/contact.js
@@ -1,1 +1,36 @@
-function sendContact() {}
+function validation() {
+  var name = document.getElementById("name").value;
+  var subject = document.getElementById("subject").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  var errorMessage = document.getElementById("errorMessage");
+  var text;
+
+  errorMessage.style.padding = "10px";
+
+  if (name.length < 5) {
+    text = "Please Enter Valid Name";
+    errorMessage.innerHTML = text;
+    return false;
+  }
+
+  if (subject.length < 5) {
+    text = "Please Enter A Subject (min. 5 characters)";
+    errorMessage.innerHTML = text;
+    return false;
+  }
+
+  if (email.indexOf("@") == -1 || email.length < 6) {
+    text = "Please Enter Valid Email";
+    errorMessage.innerHTML = text;
+    return false;
+  }
+
+  if (message.length <= 40) {
+    text = "Please Enter More Than 40 Characters";
+    errorMessage.innerHTML = text;
+    return false;
+  }
+  alert("Message sent");
+  return true;
+}
